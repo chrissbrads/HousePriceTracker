36537ee388a7084af994b519b14a085b902b2856
Chris Bradshaw <chrissbrads@gmail.com>
2023-02-16 12:26:35 +0000

    output detailed property dataframe and save to csv

--- data_wrangling/LO_EL_info.py
+++ data_wrangling/LO_EL_info.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-
 parent = 'data_files/raw/'
 cols = ['Station', 'Zone']
 liz_line_df = pd.read_csv(parent + 'Elizabeth_line_tube_stations.csv')[cols]
@@ -23,12 +22,12 @@ def trans_zone(zone):
 
 both_lines_df['zone'] = both_lines_df['zone'].apply(lambda x: trans_zone(x) if isinstance(x, str) else x)
 
-# print(both_lines_df.to_string())
-
-
 underground_df = pd.read_csv('data_files/cleaned/station_line_table.csv')[['station_name', 'line', 'zone']].rename(columns={'station_name':'station'})
-all_lines_df = pd.concat([underground_df, both_lines_df]).sort_values('station').reset_index(drop=True)
+remove_ELL = underground_df['line'] != 'East London Line'
+underground_df = underground_df.loc[remove_ELL]
+all_lines_df = pd.concat([underground_df, both_lines_df])\
+    .sort_values('station')\
+    .reset_index(drop=True)
 
-print(all_lines_df.to_string())
 
 all_lines_df.to_csv('data_files/cleaned/tfl_lines.csv', index=False)
--- get_houses_details.py
+++ get_houses_details.py
@@ -6,7 +6,7 @@ import re
 
 def get_qualifying_houses():
     houses_prov_list = get_houses_prov_list()
-    conditions = (houses_prov_list['rent_per_pers'] <= 1000) 
+    conditions = (houses_prov_list['rent_per_pers'] <= 850) 
     # & (houses_prov_list['added_status'] == 'Added today')
     return houses_prov_list.loc[conditions]
 
@@ -15,25 +15,16 @@ print(get_qualifying_houses())
 def get_house_details():
     house_df = get_qualifying_houses()
     room_list = []
-
+    stn_line_df = pd.read_csv('data_files/cleaned/tfl_lines.csv')
+    patt = re.compile('(\s*)Station$')
 
     def station_info(station):
         stn_name = station.find('span', {'class':'cGDiWU3FlTjqSs-F1LwK4'}).text
-        stn_dist = station.find('span', {'class':'_1ZY603T1ryTT3dMgGkM7Lg'}).text
+        stn_dist_str = station.find('span', {'class':'_1ZY603T1ryTT3dMgGkM7Lg'}).text
         stn_network = station.find('svg').get('data-testid').split('-')[-1]
 
-        stn_line_df = pd.read_csv('data_files/cleaned/tfl_lines.csv')
-        patt = re.compile('(\s*)Station$')
         stn_name = patt.sub('', stn_name)
-
-        # if stn_network == 'underground' or stn_network == 'overground':
-        #     tube_station = stn_line_df['station_name'] == stn_name
-        #     stn_line = stn_line_df.loc[tube_station,'line'].tolist()
-        # elif stn_network == 'networkrail':
-        #     rail_station = stn_line_df['line'] == 'Elizabeth Line' & stn_line_df['station_name'] == stn_name
-        #     stn_line = stn
-        # else:
-        #     stn_line = None
+        stn_dist = float(stn_dist_str.replace(' miles', ''))
 
         station_mask = stn_line_df['station'] == stn_name
         stn_inf = stn_line_df.loc[station_mask]
@@ -45,9 +36,9 @@ def get_house_details():
             stn_line = None
             stn_zone = None
 
-        return {'station_name': stn_name, 'distance': stn_dist, 'network':stn_network, 'line': stn_line, 'zone': stn_zone}
-    
-    loop = 0
+        return {'station_name': stn_name, 'distance (miles)': stn_dist, 'network':stn_network, 'line': stn_line, 'zone': stn_zone}
+
+
     for room in house_df.itertuples():
         url = room.url
         res = requests.get(url)
@@ -55,41 +46,46 @@ def get_house_details():
 
         letting_details_raw = soup.find('dl', {'class': '_2E1qBJkWUYMJYHfYJzUb_r'})
         station_raw = soup.find('ul', {'class':'_2f-e_tRT-PqO8w8MBRckcn'})
-        
-        # stn_name = [item.text for item in station_raw.find_all('span', {'class':'cGDiWU3FlTjqSs-F1LwK4'})]
-        # stn_dist = [item.text for item in station_raw.find_all('span', {'class':'_1ZY603T1ryTT3dMgGkM7Lg'})]
-        # stn_network = [item.get('data-testid').split('-')[-1] for item in station_raw.find_all('svg')]
-
-        # stations = list(zip(stn_name, stn_dist, stn_network))
-
         stations = list(map(station_info, station_raw.find_all('li')))
-        # stations = {item.find('span', {'class':'cGDiWU3FlTjqSs-F1LwK4'}).text: item.find('span', {'class':'_1ZY603T1ryTT3dMgGkM7Lg'}).text for item in station_raw.find_all('li')}
-        let_available = letting_details_raw.find()
+        let_available = letting_details_raw.find('div', {'class':'_2RnXSVJcWbWv4IpBC1Sng6'}).find('dd').text
+        furnished = letting_details_raw.find('div', {'class': '_2RnXSVJcWbWv4IpBC1Sng6'}).find('dd').text
+        info_reel = soup.find('div', {'class': '_4hBezflLdgDMdFtURKTWh'})
 
-        loop += 1
+        key_info = [item.text for item in info_reel.find_all('dd', {'class': '_1hV1kqpVceE9m-QrX_hWDN'})]
+        info_name = [item.text.lower() for item in info_reel.find_all('dt', {'class':'ZBWaPR-rIda6ikyKpB_E2'})]
+        info_dict = dict(zip(info_name, key_info))
+        # print(info_dict)
+        house_type = info_dict['property type']
+        
+        bathrooms = convert_nums(info_dict, 'bathrooms')
+        bedrooms = convert_nums(info_dict, 'bedrooms')
+
+        zone = min([i['zone'] for i in stations if i['zone'] is not None], default=None)
 
         details = {
-            'stations': stations
+            'house_type': house_type,
+            'bathrooms': bathrooms,
+            'available': let_available,
+            # 'furnished': furnished,
+            'stations': stations,
+            'zone': zone
         }
 
         room_list.append(details)
+    
+    return pd.DataFrame(room_list, index=house_df.index.copy())
 
 
-        # stations = []
-        # for item in station_raw.find_all('li'):
-        #     stn_name = item.find('span', {'class':'cGDiWU3FlTjqSs-F1LwK4'}).text
-        #     stn_dist = item.find('span', {'class':'_1ZY603T1ryTT3dMgGkM7Lg'}).text
-        #     stn_network = item.find('svg').get('data-testid').split('-')[-1]
+def convert_nums(my_dict, dict_field):
+    val = my_dict.get(dict_field)
+    if val:
+        val = int(my_dict[dict_field][1:])
 
-        #     info = {'station_name': stn_name, 'distance': stn_dist, 'network':stn_network}
-
-        #     stations.append(info)
-
-
-
-    return room_list, loop
+    return val
 
 print(get_house_details())
 
+merged = pd.merge(get_qualifying_houses(), get_house_details(), how='inner', left_index=True, right_index=True)
 
+merged.to_csv('data_files/results/results_list.csv')
 
